cec13c80e659d7e0929bbd020442c5af847ecf4e
Foo Bar <foo@foo.foo>
2019-08-15 15:36:30 +0300

    Задание выполнено, но кажется не совсем так, как предполагалось в задании

--- client.cpp
+++ client.cpp
@@ -6,25 +6,33 @@
 
 #include "socket_server.cpp"
 #include "socket_client.cpp"
+#include "cv_video_info.h"
 
-int type, rows, cols;
-size_t size;
-
-int main(int argc, char** argv)
+int main()
 {
-  cv::VideoCapture cap;
-
   Socket_client socket_client("/tmp/socket.video.smass");
 
+  cv_video_info* info;
+  unsigned char* raw;
+  while(!socket_client.read(raw));
+  info = (cv_video_info*)raw;
+  if (info->type != CV_8UC3) {
+    std::cerr << "Server is not broadcasting from the beginning of stream."
+                 " Restart the server." << std::endl;
+    exit(-1);
+  }
+
+  cv::VideoCapture cap;
   cv::namedWindow("ClientVideo", 1);
+
+  unsigned char* data;
   while(1) {
-    unsigned char* data2;
-    while(!socket_client.read(data2));
+    while(!socket_client.read(data));
 
-    cv::Mat frame2(720, 1280, CV_8UC3, data2);
-    imshow("ClientVideo", frame2);
+    cv::Mat frame(info->cols, info->rows, info->type, data);
+    imshow("ClientVideo", frame);
 
-    delete[] data2;
+    delete[] data;
 
     if (cv::waitKey(30) >= 0) break;
   }
--- server.cpp
+++ server.cpp
@@ -2,40 +2,45 @@
 
 #include <opencv/cv.hpp>
 #include <opencv/highgui.h>
+#include <opencv/cxcore.h>
 
 #include "socket_server.cpp"
 #include "socket_client.cpp"
+#include "cv_video_info.h"
 
-int type, rows, cols;
-size_t size;
+int size;
+int distributor = 0;
 
 int main(int argc, char** argv)
 {
-  cv::VideoCapture cap;
-  if (argc >= 2) {
-    cap.open(argv[1]);
-  } else {
-    cap.open("sample.mp4");
+  if (argc < 2) {
+    std::cerr << "Please, specify the path to the file." << std::endl;
+    exit(-1);
   }
+  cv::VideoCapture cap(argv[1]);
 
   Socket_server socket_server("/tmp/socket.video.smass");
-  Socket_client socket_client("/tmp/socket.video.smass");
 
-  cv::namedWindow("ServerVideo", 1);
+  cv_video_info info = { CV_8UC3, (int)cap.get(CV_CAP_PROP_FRAME_WIDTH),
+      (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT) };
+
+  while(!socket_server.write((unsigned char*)&info, sizeof(info)));
+
   while(1) {
     cv::Mat frame;
     cap >> frame;
 
     unsigned char* data = frame.data;
-    type = frame.type();
-    rows = frame.rows;
-    cols = frame.cols;
     size = frame.total() * frame.elemSize();
 
-    std::thread([&] {
-      while(!socket_server.write(data, size));
-    }).detach();
-
-    nanosleep((const struct timespec[]){{0, 50000000L}}, NULL);
+    if (distributor ^= 1) {
+      std::thread([&] {
+        while(!socket_server.write(data, size));
+      }).join();
+    } else {
+      std::thread([&] {
+        while(!socket_server.write(data, size));
+      }).join();
+    }
   }
 }
